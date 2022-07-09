a4e34321df1698834aceaf2c25395ca27b8867cc
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-07-09 16:36:34 +0900

    feat: setup styled-components, theme (#3)
    * chore: install styled-components
    
    install
    - styled-components
    - @types/styled-components
    
    #2
    
    * feat: Add theme, global style
    
    - 기본 테마 설정
    - 타입스크립트 적용을 위한 타입 적용
    - 글로벌스타일에 reset CSS 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import { theme } from "./styles/theme";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
